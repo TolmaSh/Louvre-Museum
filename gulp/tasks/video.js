62d22ea46847e5a2e72a08f6cf744df30f7df395
TolmaSh <m0v4anua@gmail.com>
2021-11-21 14:25:42 +0200

    feat: activate gh-pages

--- gulp/tasks/video.js
+++
@@ -1,9 +0,0 @@
-import config from '../config';
-import gulp from 'gulp';
-const video = (cb) => {
-	gulp.src(config.src.video)
-	.pipe(gulp.dest(config.build.video))
-cb()
-}
-
-export default video;
